42c8b2ebac947f2b47ff869dc78963ddfe523792
unique2721 <uniquehabeshawi@gmail.com>
2025-01-02 21:53:10 +0300

    #

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
 import { useState } from 'react'
 import { BrowserRouter,Routes,Router, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
+/* pages */
+import Home from './pages/Home'
+import Signin from './pages/Signin'
+import Signout from './pages/Signout'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import Services from './pages/Services'
+/* components */
+import Header from './components/Header'
+
 function App() {
 
   return (
     <>
       <BrowserRouter>
+      <Header/>
       <Routes>
         <Route path='/' element={<Home/>}/>
-        <Route path='/signin' element={<Sigin/>}/>
-        <Route path='/signout' element={<Signout/>}/>
         <Route path='/about' element={<About/>}/>
+        <Route path='/services' element={<Services/>}/>
+        <Route path='/signin' element={<Signin/>}/>
+        <Route path='/signout' element={<Signout/>}/>
         <Route path='/profile' element={<Profile/>}/>
       </Routes>
-        <Navbar/>
       </BrowserRouter>
     </>
   )
---
+++ src/components/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+/* react icons */
+import {FaSearch} from 'react-icons/fa'
+
+const Header = () => {
+  return (
+    <header className=' bg-slate-200 shadow-md'>
+      <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+      <h1 className=' font-bold text-sm sm:text-xl flex flex-wrap'>
+        <span className=' text-slate-500'>Bit</span>
+        <span className=' text-slate-700'>Cars</span>
+      </h1>
+      <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
+        <input type="text" placeholder='Search...' className=' bg-transparent focus:outline-none w-24 sm:w-64'/>
+        <FaSearch className=' text-slate-600'/>
+      </form>
+        <ul className='flex gap-4'>
+          <Link to={'/'} className=' hidden sm:inline text-slate-700 hover:underline'>Home</Link>
+          <Link to={'/about'} className=' hidden sm:inline text-slate-700 hover:underline'>About</Link>
+          <Link to={'/services'} className=' hidden sm:inline text-slate-700 hover:underline'>Services</Link>
+          <Link to={'/signin'} className='sm:inline text-slate-700 hover:underline'>Signin</Link>
+          <Link to={'/user'} className='sm:inline text-slate-700 hover:underline'>User</Link>
+          <Link to={'/carlisting'} className='sm:inline text-slate-700 hover:underline'>Submit lisitng</Link>
+        </ul>
+      </div>
+    </header>
+  )
+}
+
+export default Header
