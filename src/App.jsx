0467781ce5507fdeae8948ddf8490dc3c954aece
unique2721 <uniquehabeshawi@gmail.com>
2025-01-02 20:46:40 +0300

    #

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { useState } from 'react'
+import { BrowserRouter,Routes,Router, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 function App() {
 
   return (
     <>
-      <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/signin' element={<Sigin/>}/>
+        <Route path='/signout' element={<Signout/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+      </Routes>
         <Navbar/>
-      </div>
+      </BrowserRouter>
     </>
   )
 }
