2803fa4a85ed4da46028d0092f3d38a347e094fb
unique2721 <uniquehabeshawi@gmail.com>
2025-01-03 21:57:50 +0300

    #

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ import Category from '../components/Category'
 import Cars from '../components/Cars'
 import Navbar from '../components/Navbar'
 import Footer from './Footer'
+import CarFilter from '../components/CarFilter'
 
 const Home = () => {
   return (
@@ -13,6 +14,7 @@ const Home = () => {
       <Navbar/>
       <Hero/>
       <Category/>
+      <CarFilter/>
       <Cars/>
       <Footer/>
     </div>
