c9215b34a0714d5e9383a23caf6e872f48e4ec3d
unique2721 <uniquehabeshawi@gmail.com>
2025-01-03 19:45:15 +0300

    #

--- src/components/Cars.jsx
+++ src/components/Cars.jsx
@@ -2,20 +2,21 @@ import React from 'react'
 import Data from '../Data/Data'
 const Cars = () => {
   return (
-    <div>
+    <div >
       <h2 className=' font-bold text-3xl text-center my-16'>Most Searched Cars</h2>
+      <div className='grid grid-cols-3 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-9 gap-6'>
+
       {
         Data.Cars.map((car,index) => (
-          <div key={index} className='grid grid-cols-3 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-9 gap-6'>
-            <div>
+          <div key={index}> 
             <img src={car.img} alt="carimg" width={300} height={250} className=' rounded-t-xl bg-black ' />
             <div className=' p-4'>
               <h2 className=' font-bold text-black text-lg mb-2'>{car.name}</h2>
-             </div>
             </div>
           </div>
         ))
       }
+      </div>
     </div>
   )
 }
